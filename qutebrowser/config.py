cd2c23204625c0d2b9d59eee1f9f42d2f68a150c
Jack Davidson <jackmaverickdavidson@gmail.com>
2020-12-12 12:09:15 -0600

    fixed kill

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,4 +1,5 @@
 config.source("nord-qutebrowser.py")
+
 c.downloads.location.directory = "~/dow"
 c.url.start_pages = "file:///home/jd/.config/qutebrowser/homepage.html"
 c.url.default_page = "file:///home/jd/.config/qutebrowser/homepage.html"
