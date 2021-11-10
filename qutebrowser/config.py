9505276aa90f6fc0a57f3911812422fe06a2118c
Jack Davidson <jackmaverickavidson@gmail.com>
2021-11-10 14:43:39 -0600

    added userscripts and added config.py entry to hook up the userscripts to keybinds within the browser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -3,5 +3,8 @@ config.load_autoconfig()
 c.downloads.location.directory = "~/dow"
 c.url.default_page = "file:///home/jd/.config/qutebrowser/homepage.html"
 c.url.start_pages = "file:///home/jd/.config/qutebrowser/homepage.html"
+c.content.tls.certificate_errors = "ask-block-thirdparty"
+config.bind("\\d", "spawn -u dict")
+config.bind("\\w", "spawn -u wiki")
 
 config.source('nord-qutebrowser.py')
