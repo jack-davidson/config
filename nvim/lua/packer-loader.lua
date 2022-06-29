600e73449d9031e95131fbb9e5741f148ff14f9e
Jack Davidson <jackmaverickavidson@gmail.com>
2022-06-28 21:18:44 -0500

    updated packer loader to be shorter

--- nvim/lua/packer-loader.lua
+++ nvim/lua/packer-loader.lua
@@ -4,17 +4,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-local packer = nil
-if packer == nil then
-    packer = require 'packer'
-    packer.init { disable_commands = true }
-end
+packer = require 'packer'
+packer.init { disable_commands = true }
 
-local use = packer.use
-use 'wbthomason/packer.nvim'
-require('plugins')(use)
--- Automatically set up your configuration after cloning packer.nvim
--- Put this at the end after all config
+packer.use 'wbthomason/packer.nvim'
+require('plugins')(packer.use) -- pass packer.use to plugin function
+
 if packer_bootstrap then
         require('packer').sync()
 end
