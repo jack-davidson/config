4f1b5989b2c585ad6b5dffb3053d28c493599621
Jack Davidson <jackmaverickavidson@gmail.com>
2022-06-30 18:44:29 -0500

    qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,10 +1,11 @@
-config.load_autoconfig()
-
 c.fonts.default_family = "Fira Code Nerd Font"
 c.downloads.location.directory = "~/dow"
+c.content.javascript.can_access_clipboard = True
+c.colors.webpage.preferred_color_scheme = True
+c.content.tls.certificate_errors = "ask-block-thirdparty"
 c.url.default_page = "file:///home/jd/.config/qutebrowser/homepage.html"
 c.url.start_pages = "file:///home/jd/.config/qutebrowser/homepage.html"
-c.content.tls.certificate_errors = "ask-block-thirdparty"
+
 config.bind("\\d", "spawn -u dict")
 config.bind("\\w", "spawn -u wiki")
 
