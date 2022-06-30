2ce259a57287aef168038ae72ce23fe6211f8ec1
Jack Davidson <jackmaverickavidson@gmail.com>
2022-06-30 18:44:53 -0500

    qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,7 @@
 c.fonts.default_family = "Fira Code Nerd Font"
 c.downloads.location.directory = "~/dow"
 c.content.javascript.can_access_clipboard = True
-c.colors.webpage.preferred_color_scheme = True
+c.colors.webpage.preferred_color_scheme = "dark"
 c.content.tls.certificate_errors = "ask-block-thirdparty"
 c.url.default_page = "file:///home/jd/.config/qutebrowser/homepage.html"
 c.url.start_pages = "file:///home/jd/.config/qutebrowser/homepage.html"
