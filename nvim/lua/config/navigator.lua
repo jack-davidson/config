109b252a2cb2f0ca2f52866f0ba04e3c0b00d7eb
Jack Davidson <jackmaverickavidson@gmail.com>
2022-06-30 20:21:36 -0500

    seamless navigation between vim and tmux

--- nvim/lua/config/navigator.lua
+++ nvim/lua/config/navigator.lua
@@ -1,7 +1,7 @@
 require('Navigator').setup()
 -- Keybindings
-vim.keymap.set('n', "<A-h>", '<CMD>NavigatorLeft<CR>')
-vim.keymap.set('n', "<A-l>", '<CMD>NavigatorRight<CR>')
-vim.keymap.set('n', "<A-k>", '<CMD>NavigatorUp<CR>')
-vim.keymap.set('n', "<A-j>", '<CMD>NavigatorDown<CR>')
-vim.keymap.set('n', "<A-p>", '<CMD>NavigatorPrevious<CR>')
+vim.keymap.set('n', "<C-w>h", '<CMD>NavigatorLeft<CR>')
+vim.keymap.set('n', "<C-w>l", '<CMD>NavigatorRight<CR>')
+vim.keymap.set('n', "<C-w>k", '<CMD>NavigatorUp<CR>')
+vim.keymap.set('n', "<C-w>j", '<CMD>NavigatorDown<CR>')
+vim.keymap.set('n', "<C-w>p", '<CMD>NavigatorPrevious<CR>')
