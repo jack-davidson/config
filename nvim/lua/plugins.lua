0a727aae834fb05f8e071b27eb9124e765eac1e2
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-03 10:39:35 -0500

    updated plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,39 +4,37 @@ return function(use)
     -- Tools
     use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim', config = require('config.telescope'), }
     use { 'numToStr/Navigator.nvim', config = require('config.navigator'), }
-    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-    use { 'kdheepak/lazygit.nvim', }
+    use { 'kdhepak/lazygit.nvim', }
 
     -- LSP
-    use { 'folke/trouble.nvim', config = require('config.trouble'), }
     use { 'neovim/nvim-lspconfig', config = require('config.lsp'), }
-    use { 'hrsh7th/nvim-cmp', config = require('config.cmp'), }
-    use { 'hrsh7th/cmp-nvim-lsp', }
-
-    -- nvim-cmp Sources
-    use { 'hrsh7th/cmp-nvim-lsp-signature-help', }
-    use { 'hrsh7th/cmp-buffer', }
-    use { 'hrsh7th/cmp-vsnip', }
-    use { 'hrsh7th/cmp-path', }
+    use { 'hrsh7th/nvim-cmp', requires = {
+        -- completion sources
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-vsnip',
+        'hrsh7th/cmp-path',
+    }, config = require('config.cmp'), }
 
     -- Snippets
+    use { 'rafamadriz/friendly-snippets', }
     use { 'hrsh7th/vim-vsnip-integ', }
     use { 'hrsh7th/vim-vsnip', }
 
     -- Language
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = require('config.treesitter'), }
-    use { 'norcalli/nvim-colorizer.lua', config = require('config.colorizer'), }
-    use { 'rafamadriz/friendly-snippets', }
     use { 'windwp/nvim-ts-autotag', }
-    use { 'fatih/vim-go', }
 
-    -- UI/Appearance
+    -- UI
     use { 'lukas-reineke/indent-blankline.nvim', config = require('config.indent_blankline'), }
+    use { 'norcalli/nvim-colorizer.lua', config = require('config.colorizer'), }
     use { 'akinsho/bufferline.nvim', config = require('config.bufferline'), }
     use { 'startup-nvim/startup.nvim', config = require('config.startup'), }
     use { 'nvim-lualine/lualine.nvim', config = require('config.lualine'), }
     use { 'lewis6991/gitsigns.nvim', config = require('config.gitsigns'), }
+
+    -- Colorschemes
     use { 'shaunsingh/nord.nvim', config = require('config.nord'), }
     use { 'ellisonleao/gruvbox.nvim' }
-    use { 'crispgm/nvim-tabline', }
 end
