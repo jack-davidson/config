bb771d52add2a642972484fccf1149f6b49339e5
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-05 13:29:58 -0500

    pdfjs and protocol handlers

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,14 +6,20 @@ c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
 
 config.load_autoconfig(False)
 
-c.downloads.location.directory = "~/dow"
 c.content.javascript.can_access_clipboard = True
-c.colors.webpage.preferred_color_scheme = "dark"
 c.content.tls.certificate_errors = "ask-block-thirdparty"
+
 c.url.default_page = "file:///home/jd/.config/qutebrowser/homepage.html"
 c.url.start_pages = "file:///home/jd/.config/qutebrowser/homepage.html"
+c.downloads.location.directory = "~/dow"
+
+config.set('content.notifications.enabled', True, 'https://calendar.google.com/*')
+
+c.content.pdfjs = True
+c.content.register_protocol_handler = True
 
 config.bind("\\d", "spawn -u dict")
 config.bind("\\w", "spawn -u wiki")
 
+c.colors.webpage.preferred_color_scheme = "dark"
 config.source('nord-qutebrowser.py')
