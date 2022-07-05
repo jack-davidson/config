1e420748bf851e044765747965efa72e892624ba
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-05 13:06:11 -0500

    mimeapps.list

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,3 +1,9 @@
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
 config.load_autoconfig(False)
 
 c.downloads.location.directory = "~/dow"
