6b9d7ce3f648976a9efc46ebd8fe6a4fc21bc11e
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-06 19:51:50 -0500

    change order of plugins to make more sense

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,15 +1,17 @@
 return function(use)
     use { 'wbthomason/packer.nvim', }
 
-    -- Tools
+    -- Tools/Utility
     use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim', config = require('config.telescope'), }
+    use { 'kyazdani42/nvim-tree.lua', config = require('config.nvim-tree'), }
     use { 'numToStr/Navigator.nvim', config = require('config.navigator'), }
+    use { 'linty-org/readline.nvim', config = require('config.readline'), }
+    use { 'numToStr/Comment.nvim', config = require('Comment').setup(), }
+    use { 'simrat39/symbols-outline.nvim', }
     use { 'kdheepak/lazygit.nvim', }
-    use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons', config = require('config.nvim-tree'), }
 
     -- LSP
     use { 'neovim/nvim-lspconfig', config = require('config.lsp'), }
-    use { 'simrat39/symbols-outline.nvim', }
     use { 'hrsh7th/nvim-cmp', requires = {
         -- completion sources
         'hrsh7th/cmp-nvim-lsp-signature-help',
@@ -29,12 +31,10 @@ return function(use)
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = require('config.treesitter'), }
     use { 'windwp/nvim-ts-autotag', }
 
-    -- UI
-    use { 'numToStr/Comment.nvim', config = require('Comment').setup(), }
-    use { 'linty-org/readline.nvim', config = require('config.readline'), }
-    use { "petertriho/nvim-scrollbar", config = require('scrollbar').setup{} }
+    -- UI/Visuals
     use { 'lukas-reineke/indent-blankline.nvim', config = require('config.indent_blankline'), }
     use { 'norcalli/nvim-colorizer.lua', config = require('config.colorizer'), }
+    use { "petertriho/nvim-scrollbar", config = require('scrollbar').setup{} }
     use { 'akinsho/bufferline.nvim', config = require('config.bufferline'), }
     use { 'startup-nvim/startup.nvim', config = require('config.startup'), }
     use { 'nvim-lualine/lualine.nvim', config = require('config.lualine'), }
