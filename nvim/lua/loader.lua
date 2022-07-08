1a64d71a69913d9f1c367a99fc9dc3c8a7a1a443
Jack Davidson <jackmaverickdavidson@gmail.com>
2022-07-08 12:34:44 -0500

    remove bootstrapping

--- nvim/lua/loader.lua
+++ nvim/lua/loader.lua
@@ -1,13 +1,3 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
 require('packer').startup(function(spec)
     require('plugins')(use) -- pass use to plugin function
 end)
-
-if packer_bootstrap then
-        require('packer').sync()
-end
