ce57212e71d70e44fb48571fae96f4944253f4dd
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-09 11:42:38 -0500

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+return {
+    color_scheme = "nord",
+    colors = {
+        tab_bar = {
+            background = "#2e3440",
+            active_tab = {
+                bg_color = "#5e81ac",
+                fg_color = "#eceff4",
+                intensity = "Bold",
+                italic = true
+            },
+            inactive_tab = {bg_color = "#4c566a", fg_color = "#d8dee9"},
+            inactive_tab_hover = {
+                bg_color = "#d8dee9",
+                fg_color = "#3b4252",
+                italic = false
+            },
+            new_tab = {bg_color = "#3b4252", fg_color = "#a3be8c"},
+            new_tab_hover = {
+                bg_color = "#3b4252",
+                fg_color = "#8fbcbb",
+                italic = false
+            }
+        }
+    },
+    line_height = 1,
+}
