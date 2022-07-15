8f3031cb5a6d52eff70d9f9aeb8d1858d6878207
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-15 13:45:02 -0500

    updated scripts

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+host=$(cat /etc/hostname)
+
+pwd=$(pwd)
+
+dir() {
+    ln -sf $pwd/$1 $2
+}
+
+files() {
+    for file in $(ls $1); do
+        ln -sf $pwd/$1/$file $2/$file
+    done;
+}
+
+dotfiles() {
+    for file in $(ls $1); do
+        ln -sf $pwd/$1/$file $2$file
+    done;
+}
+
--- install.sh
+++ install.sh
@@ -1,24 +1,4 @@
-#!/bin/sh
-
-host=$(cat /etc/hostname)
-
-pwd=$(pwd)
-
-dir() {
-    ln -sf $pwd/$1 $2
-}
-
-files() {
-    for file in $(ls $1); do
-        ln -sf $pwd/$1/$file $2/$file
-    done;
-}
-
-dotfiles() {
-    for file in $(ls $1); do
-        ln -sf $pwd/$1/$file $2$file
-    done;
-}
+. ./init.sh
 
 dir     qutebrowser     ~/.config
 dir     alacritty       ~/.config
