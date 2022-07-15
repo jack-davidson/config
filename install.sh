97cb1595feb365960f400486a462852925c570c5
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-15 13:06:55 -0500

    moved mimeapps.list

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+host=$(cat /etc/hostname)
+
+pwd=$(pwd)
+
+dir() {
+    ln -sf $pwd/$1 $2
+    echo "$pwd/$1 -> $2"
+}
+
+files() {
+    for file in $(ls $1); do
+        ln -sf $pwd/$1/$file $2/$file
+        echo "$pwd/$1/$file -> $2/$file"
+    done;
+}
+
+dotfiles() {
+    for file in $(ls $1); do
+        ln -sf $pwd/$1/$file $2$file
+        echo "$pwd/$1/$file -> $2$file"
+    done;
+}
+
+dir     qutebrowser     ~/.config
+dir     alacritty       ~/.config
+dir     kitty           ~/.config
+dir     fontconfig      ~/.config
+dir     nvim            ~/.config
+dir     zathura         ~/.config
+dir     dunst           ~/.config
+dir     spotifyd        ~/.config
+dir     spotify-tui     ~/.config
+dir     picom           ~/.config
+dir     mpd:host        ~/.config
+dir     ncmpcpp         ~/.config
+dir     wezterm         ~/.config
+
+dotfiles   xinit:$host     ~/.
+dotfiles   alsa:$host      ~/.
+
+files   xdg             ~/.config
+
+files   tmux    ~/.
+files   zsh     ~/.
