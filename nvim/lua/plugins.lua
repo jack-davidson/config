6892c9cff0792c01b7ff659ec7344023fe4f8e0e
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-15 23:03:12 -0500

    fix plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,16 +8,16 @@ require('packer').startup(function(use)
     use { 'wbthomason/packer.nvim', }
 
     -- Tools/Utility
-    use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim', config = require('config.telescope'), }
-    use { 'kyazdani42/nvim-tree.lua', config = require('config.nvim-tree'), }
-    use { 'numToStr/Navigator.nvim', config = require('config.navigator'), }
-    use { 'linty-org/readline.nvim', config = require('config.readline'), }
-    use { 'numToStr/Comment.nvim', config = require('Comment').setup(), }
+    use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim', config = function() require('config.telescope') end, }
+    use { 'kyazdani42/nvim-tree.lua', config = function() require('config.nvim-tree') end, }
+    use { 'numToStr/Navigator.nvim', config = function() require('config.navigator') end, }
+    use { 'linty-org/readline.nvim', config = function() require('config.readline') end, }
+    use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end, }
     use { 'simrat39/symbols-outline.nvim', }
     use { 'kdheepak/lazygit.nvim', }
 
     -- LSP
-    use { 'neovim/nvim-lspconfig', config = require('config.lsp'), }
+    use { 'neovim/nvim-lspconfig', config = function() require('config.lsp') end, }
     use { 'hrsh7th/nvim-cmp', requires = {
         -- completion sources
         'hrsh7th/cmp-nvim-lsp-signature-help',
@@ -26,7 +26,7 @@ require('packer').startup(function(use)
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-vsnip',
         'hrsh7th/cmp-path',
-    }, config = require('config.cmp'), }
+    }, config = function() require('config.cmp') end, }
 
     -- Snippets
     use { 'rafamadriz/friendly-snippets', }
@@ -34,21 +34,21 @@ require('packer').startup(function(use)
     use { 'hrsh7th/vim-vsnip', }
 
     -- Language
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = require('config.treesitter'), }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function() require('config.treesitter') end, }
     use { 'windwp/nvim-ts-autotag', }
 
     -- UI/Visuals
-    use { 'lukas-reineke/indent-blankline.nvim', config = require('config.indent_blankline'), }
-    use { 'norcalli/nvim-colorizer.lua', config = require('config.colorizer'), }
-    use { "petertriho/nvim-scrollbar", config = function() require('scrollbar').setup{} end }
-    use { 'akinsho/bufferline.nvim', config = require('config.bufferline'), }
-    use { 'startup-nvim/startup.nvim', config = require('config.startup'), }
-    use { 'nvim-lualine/lualine.nvim', config = require('config.lualine'), }
-    use { 'lewis6991/gitsigns.nvim', config = require('config.gitsigns'), }
+    use { 'lukas-reineke/indent-blankline.nvim', config = function() require('config.indent_blankline') end, }
+    use { 'norcalli/nvim-colorizer.lua', config = function() require('config.colorizer') end, }
+    use { "petertriho/nvim-scrollbar", config = function() require('scrollbar').setup{} end, }
+    use { 'akinsho/bufferline.nvim', config = function() require('config.bufferline') end, }
+    use { 'startup-nvim/startup.nvim', config = function() require('config.startup') end, }
+    use { 'nvim-lualine/lualine.nvim', config = function() require('config.lualine') end, }
+    use { 'lewis6991/gitsigns.nvim', config = function() require('config.gitsigns') end, }
     use { 'kyazdani42/nvim-web-devicons', }
 
     -- Colorschemes
-    use { 'shaunsingh/nord.nvim', config = require('config.nord'), }
+    use { 'shaunsingh/nord.nvim', config = function() require('config.nord') end, }
     use { 'ellisonleao/gruvbox.nvim' }
 
     if packer_bootstrap then
