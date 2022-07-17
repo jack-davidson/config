f53c5343f272bb617f8676bdccf38d1672d8671b
Jack Davidson <jackmaverickavidson@gmail.com>
2022-07-17 07:47:41 -0500

    clean up use statements

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,53 +5,55 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-    use { 'wbthomason/packer.nvim', }
+    use 'wbthomason/packer.nvim'
     use 'nvim-lua/plenary.nvim'
 
     -- Tools/Utility
     use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim',  }
-    use { 'kyazdani42/nvim-tree.lua',  }
-    use { 'numToStr/Navigator.nvim',  }
-    use { 'linty-org/readline.nvim',  }
-    use { 'numToStr/Comment.nvim',  }
-    use { 'simrat39/symbols-outline.nvim', }
-    use { 'kdheepak/lazygit.nvim', }
-    use{ 'jakewvincent/mkdnflow.nvim', }
+    use 'kyazdani42/nvim-tree.lua'
+    use 'numToStr/Navigator.nvim'
+    use 'linty-org/readline.nvim'
+    use 'numToStr/Comment.nvim'
+    use 'simrat39/symbols-outline.nvim'
+    use 'kdheepak/lazygit.nvim'
+    use 'jakewvincent/mkdnflow.nvim'
 
     -- LSP
-    use { 'neovim/nvim-lsp' }
-    use { 'hrsh7th/nvim-cmp', requires = {
-        -- completion sources
-        'hrsh7th/cmp-nvim-lsp-signature-help',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-vsnip',
-        'hrsh7th/cmp-path',
-    },  }
+    use 'neovim/nvim-lsp'
+    use { 'hrsh7th/nvim-cmp',
+        requires = {
+            -- completion sources
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-vsnip',
+            'hrsh7th/cmp-path',
+        }
+    }
 
     -- Snippets
-    use { 'rafamadriz/friendly-snippets', }
-    use { 'hrsh7th/vim-vsnip-integ', }
-    use { 'hrsh7th/vim-vsnip', }
+    use 'rafamadriz/friendly-snippets'
+    use 'hrsh7th/vim-vsnip-integ'
+    use 'hrsh7th/vim-vsnip'
 
     -- Language
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',  }
-    use { 'windwp/nvim-ts-autotag', }
+    use 'windwp/nvim-ts-autotag'
 
     -- UI/Visuals
-    use { 'lukas-reineke/indent-blankline.nvim',  }
-    use { 'norcalli/nvim-colorizer.lua',  }
-    use { "petertriho/nvim-scrollbar", }
-    use { 'akinsho/bufferline.nvim',  }
-    use { 'startup-nvim/startup.nvim',  }
-    use { 'nvim-lualine/lualine.nvim',  }
-    use { 'lewis6991/gitsigns.nvim', }
-    use { 'kyazdani42/nvim-web-devicons', }
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'petertriho/nvim-scrollbar'
+    use 'akinsho/bufferline.nvim'
+    use 'startup-nvim/startup.nvim'
+    use 'nvim-lualine/lualine.nvim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'kyazdani42/nvim-web-devicons'
 
     -- Colorschemes
-    use { 'shaunsingh/nord.nvim',  }
-    use { 'ellisonleao/gruvbox.nvim' }
+    use 'shaunsingh/nord.nvim'
+    use 'ellisonleao/gruvbox.nvim'
 
     if packer_bootstrap then
         require('packer').sync()
